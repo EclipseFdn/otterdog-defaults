f449b6c6c90ac622b355405759476dc29a76d0a3
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2023-08-31 19:28:35 +0200

    Add additional validation rules for sensitive org settings.

--- validate-org-settings.py
+++ validate-org-settings.py
@@ -3,4 +3,6 @@
 context.property_equals(self, "default_repository_permission", "none")
 context.property_equals(self, "members_can_create_public_repositories", False)
 context.property_equals(self, "members_can_create_private_repositories", False)
+context.property_equals(self, "members_can_change_repo_visibility", False)
+context.property_equals(self, "members_can_delete_repositories", False)
 context.property_equals(self, "members_can_create_teams", False)
