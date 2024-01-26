8dd287ab2638ff671e310ea844f9fc9307181802
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-01-26 10:51:19 +0100

    churn: use project name rather than eclipse_project property which got removed, check for non-existant projects

--- pre-add-object-hook.py
+++ pre-add-object-hook.py
@@ -7,10 +7,10 @@ from otterdog.utils import print_warn
 from otterdog.models.repository import Repository
 
 if isinstance(model_object, Repository):
-    eclipse_project = org_config.eclipse_project
+    project_name = org_config.name
     repository = cast(Repository, model_object)
 
-    response = requests.get(f"https://projects.eclipse.org/api/projects/{eclipse_project}")
+    response = requests.get(f"https://projects.eclipse.org/api/projects/{project_name}")
 
     has_github_org = False
     if response.status_code == 200:
@@ -22,10 +22,12 @@ if isinstance(model_object, Repository):
                 if github_org is not None and len(github_org) > 0:
                     has_github_org = True
 
-    if has_github_org is False:
-        print_warn(
-            f"Adding repository '{repository.name}' while the project '{eclipse_project}' has no "
-            f"GitHub organization configured in the PMI.\n"
-            f"Don't forget to add an entry for this repo to the list of GitHub repositories: \n"
-            f"  https://github.com/{org_config.github_id}/{repository.name}"
-        )
+        if has_github_org is False:
+            print_warn(
+                f"Adding repository '{repository.name}' while the project '{project_name}' has no "
+                f"GitHub organization configured in the PMI.\n"
+                f"Don't forget to add an entry for this repo to the list of GitHub repositories: \n"
+                f"  https://github.com/{org_config.github_id}/{repository.name}"
+            )
+    elif response.status_code == 404:
+        print_warn(f"No Eclipse project found for project with name '{project_name}'.")
