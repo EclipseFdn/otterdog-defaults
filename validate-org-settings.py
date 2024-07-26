f6fd784ee5eb8a367c06e3791c89a5faced8895e
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2024-07-26 20:47:15 +0200

    feat: disable validation for certain properties for foundation internal orgs

--- validate-org-settings.py
+++ validate-org-settings.py
@@ -1,10 +1,7 @@
 # some EF specific validation rules for organization settings:
 
-context.property_equals(self, "default_repository_permission", "none")
-context.property_equals(self, "billing_email", "webmaster@eclipse-foundation.org")
 context.property_equals(self, "members_can_create_public_repositories", False)
 context.property_equals(self, "members_can_create_private_repositories", False)
 context.property_equals(self, "members_can_change_repo_visibility", False)
 context.property_equals(self, "members_can_delete_repositories", False)
-context.property_equals(self, "members_can_create_teams", False)
 context.property_equals(self, "default_code_security_configurations_disabled", True)
